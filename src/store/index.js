dc3b78819117414f68833a4c9ce357e9a27352ae
komaljituri12 <komal.jituri@pristyncare.com>
2024-08-21 11:54:04 +0530

    Updates

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import recipeReducer from "../slice/recipe";
-
-export const store = configureStore({
-    reducer: {
-        'recipe': recipeReducer
-    }
-})
